eac33e0a7b7187572871744b663e2c842481a416
jminsausti <jminsausti@fptxurdinaga.com>
2023-11-29 18:39:56 +0100

    Ejer1 done

---
+++ Ejer1/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Ejercicio1 from './components/Ejercicio1.vue'
+</script>
+
+<template>
+  <h1>Ejericio 1</h1>
+
+  <Ejercicio1 />
+
+
+  
+</template>
+
+<style scoped>
+div{
+  margin: 50px;
+  text-align: center;
+}
+</style>
---
+++ Ejer1/src/components/Ejercicio1.vue
@@ -0,0 +1,47 @@
+<script>
+  export default {
+      name: 'Ejercicio1',
+      components: {},
+      data() {
+        return {
+          contador: 0,
+          inicio: 0,
+          x: 0,
+          y: 0
+        }
+    },
+    methods: {
+      sumar(dos){
+        this.contador += dos;
+      },
+      cambioContador(){
+        this.contador = this.inicio;
+      },
+      valoresXY(evento){
+        this.x = evento.clientX;
+        this.y = evento.clientY;
+      }
+    },
+  }
+</script>
+
+
+<template>
+  <input type="text" v-model.number="inicio" @keyup="cambioContador">
+    <button @click="sumar(2)">Sumar + 2</button>
+    <p v-text="contador"></p>
+    <p @mousemove="valoresXY">
+      valor de x: {{x}} <br>
+      valor de y: {{y}}
+    </p>
+</template>
+
+<style scoped>
+template{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 2em;
+}
+</style>
