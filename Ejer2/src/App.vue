6e9ce7c4e9e3e20a49d5a58d45a61d8f00ce43df
jminsausti <jminsausti@fptxurdinaga.com>
2023-11-29 18:59:02 +0100

    Ejer2 done

---
+++ Ejer2/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Ejercicio2 from './components/Ejercicio2.vue'
+</script>
+
+<template>
+  <header>
+    <div >
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+      <Ejercicio2 />
+    </div>
+  </header>
+
+  
+</template>
+
+<style scoped>
+div{
+  margin: 50px;
+  text-align: center;
+}
+</style>
---
+++ Ejer2/src/components/Ejercicio2.vue
@@ -0,0 +1,61 @@
+<script>
+  export default {
+      name: 'Ejercicio2',
+      data() {
+        return {
+            tareas: [],
+            mostrar: false
+        }
+      },
+      methods: {
+            nuevaTrea(){
+              let tareasId = document.getElementById("tareasId").value;
+              this.tareas.push(tareasId);
+              this.mostrar = true;
+              document.getElementById("tareasId").value=""
+            }
+      },
+  }
+</script>
+
+
+<template>
+  <div id="app">
+    <h1>Lista de Tareas por Realizar</h1>
+    <h2>Crea una nueva tarea:</h2>
+    <p>Tarea<input type="text" id="tareasId" @keyup.enter="nuevaTrea"><button @click="nuevaTrea">Crear</button></p>
+    <h2>Lista</h2>
+    <ul v-show="mostrar">
+      <li v-for="(item, index) in tareas" :key="index">{{item}}</li>
+    </ul>
+    <p v-show="!mostrar">Aun no se han cargado tareas por realizar</p>
+  </div> 
+</template>
+
+<style scoped>
+div{
+  text-align: center;
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
