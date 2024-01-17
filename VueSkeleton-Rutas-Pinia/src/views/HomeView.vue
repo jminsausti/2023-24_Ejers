07d764f4dbde3cd454dec2ba3ff2afb41d5ff1d5
jose <jminsausti@fptxurdinaga.com>
2024-01-17 13:38:06 +0100

    VueSkeleton done

---
+++ VueSkeleton-Rutas-Pinia/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+export default{
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
